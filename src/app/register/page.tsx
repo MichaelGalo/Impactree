72b13e6a4274d5a6b12b944e626edaf734337a60
Michael Galo <michael@michaelthinks.co>
2024-10-18 09:19:18 -0500

    Added toggle password viewing on registration

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,30 +1,107 @@
+"use client"
+
+import { useState } from "react"
+
 const Register = () => {
-    // FIXME: Need to Add Button Handler Functions for Register & Redirect from Already have an account
+    const [username, setUsername] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+    
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const registrationRequest: object = {
+            username: username,
+            first_Name: firstName, 
+            last_Name: lastName,
+            email: email,
+            password: password
+        }
+        console.log(registrationRequest)
+
+        // invoke registration fetch call
+
+        // clear fields
+
+        // navigate to home page with new auth
+
+    }
 
 
     return (
         <div className="container mx-auto mt-10 max-w-md">
             <h1 className="text-2xl font-bold mb-5 text-center">Register Your Account</h1>
-            <form className="space-y-4">
+            <form onSubmit={submit} className="space-y-4">
                 <div>
                     <label htmlFor="username" className="block mb-1">Username</label>
-                    <input type="text" id="username" name="username" className="w-full border rounded p-2" />
+                    <input 
+                        type="text" 
+                        id="username" 
+                        name="username" 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        className="w-full border rounded p-2 dark:text-gray-800" 
+                    />
                 </div>
                 <div>
                     <label htmlFor="firstName" className="block mb-1">First Name</label>
-                    <input type="text" id="firstName" name="firstName" className="w-full border rounded p-2" />
+                    <input 
+                        type="text" 
+                        id="firstName" 
+                        name="firstName" 
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        className="w-full border rounded p-2 dark:text-gray-800" 
+                    />
                 </div>
                 <div>
                     <label htmlFor="lastName" className="block mb-1">Last Name</label>
-                    <input type="text" id="lastName" name="lastName" className="w-full border rounded p-2" />
+                    <input 
+                        type="text" 
+                        id="lastName" 
+                        name="lastName" 
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        className="w-full border rounded p-2 dark:text-gray-800" 
+                    />
                 </div>
                 <div>
                     <label htmlFor="email" className="block mb-1">Email</label>
-                    <input type="email" id="email" name="email" className="w-full border rounded p-2" />
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="w-full border rounded p-2 dark:text-gray-800" 
+                    />
                 </div>
                 <div>
+                    <div className="relative">
                     <label htmlFor="password" className="block mb-1">Password</label>
-                    <input type="password" id="password" name="password" className="w-full border rounded p-2" />
+                    <input 
+                        type={showPassword ? "text" : "password"}
+                        id="password" 
+                        name="password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="w-full border rounded p-2 pr-10 dark:text-gray-800" 
+                    />
+                    <button
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                        className="absolute inset-y-0 right-0 top-6 pr-3 flex items-center text-gray-600 hover:text-gray-800"
+                    >
+                        {showPassword ? "🫣" : "👀"}
+                    </button>
+                </div>
                 </div>
                 <div className="text-center">
                     <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
