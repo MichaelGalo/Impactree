82f2ccbf5c532258ca0cc943cc3c20efbd75dd0f
Michael Galo <michael@michaelthinks.co>
2024-10-22 09:35:13 -0500

    pseudocoded explore

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,22 @@
+// SSR or CSR?
+
+// Act as a charities card list view
+
+const Explore = () => {
+    // import user profile to determine if an admin (changes buttons)
+    // charities state
+    // isLoading state?
+    // Loading message state
+
+    // useEffect to get charities & set them to variable. Also include a .catch to setLoadingMessage
+
+    // title banner: "Browse and Support Your Favorite Cause"
+    // filter button (to be imported from components): Filter by Cause
+    // admin only button: "Add Organization"
+    // map through charity cards (import card component)
+    
+
+}
+
+
+export default Explore
