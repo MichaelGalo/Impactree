567c38006c4bb74740a1483e25baa101289d3a15
Michael Galo <michael@michaelthinks.co>
2024-10-23 15:33:08 -0500

    Added blank profile page for button redirect

--- src/app/impactplans/page.tsx
+++ src/app/impactplans/page.tsx
@@ -92,7 +92,9 @@ const unallocatedFunds = (impactPlan: ImpactPlan): string => {
         >
           Plan Settings
         </button>
-        <button className="px-4 py-2 border rounded-lg bg-white dark:bg-gray-800 dark:text-gray-200 border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400">
+        <button className="px-4 py-2 border rounded-lg bg-white dark:bg-gray-800 dark:text-gray-200 border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
+        onClick={()=>{router.push(`/profile/${userProfile?.id}`)}}
+        >
           Profile Settings
         </button>
       </div>
