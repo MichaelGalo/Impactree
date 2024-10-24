c9f0c92c7bf43b0fdeb590910ee7e06345662012
Michael Galo <michael@michaelthinks.co>
2024-10-24 10:09:08 -0500

    Added Nextui library and converted nav bar

---
+++ src/providers/nextui-provider.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { NextUIProvider } from "@nextui-org/react"
+
+export function NextUIAppProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  )
+}
