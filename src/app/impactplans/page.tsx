94464c659c678ecd72934c7a8fdad3651e6fc3ad
Michael Galo <michael@michaelthinks.co>
2024-10-28 11:32:04 -0500

    service fn & profile component now updating user profile

--- src/app/impactplans/page.tsx
+++ src/app/impactplans/page.tsx
@@ -63,7 +63,7 @@ const ImpactDashboard = () => {
           Plan Settings
         </button>
         <button className="px-4 py-2 border rounded-lg bg-white dark:bg-gray-800 dark:text-gray-200 border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
-        onClick={()=>{router.push(`/profile/${userProfile?.id}`)}}
+        onClick={()=>{router.push(`/users/${userProfile?.id}`)}}
         >
           Profile Settings
         </button>
