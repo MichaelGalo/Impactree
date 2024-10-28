c10297126371fbfc61dfb86145dce225e87442fa
Michael Galo <michael@michaelthinks.co>
2024-10-28 13:40:15 -0500

    admin users can now create charities

--- src/services/charity.ts
+++ src/services/charity.ts
@@ -1,5 +1,6 @@
 import { Charity } from "@/types/charity.types";
 import { fetchWithResponse } from "./fetcher";
+import { CharityFormData } from "@/app/charities/create/page";
 
 export const getCharities = async () => {
   return await fetchWithResponse<Charity[]>("charities", {
@@ -15,4 +16,15 @@ export const getCharityById = async (charityId: number) => {
         Authorization: `Token ${localStorage.getItem('token')}`
       }
     })
+}
+
+export const createCharity = async (formData: CharityFormData) => {
+  return await fetchWithResponse<Charity>("charities", {
+    method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify(formData)
+  })
 }
